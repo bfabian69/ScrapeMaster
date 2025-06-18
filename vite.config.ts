a9d9801cae410848acb8a50402466bc5a3f00204
bfabian69 <bfabian@boxcarcreative.com>
2025-06-17 21:20:52 -0400

    Fix Vite Development Server

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: '.',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  build: {
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html')
-    }
+  server: {
+    port: 5173,
+    host: true,
+    open: true
   }
 });
